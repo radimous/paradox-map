c97e68d46b48621a3cda4f0f6c910b1baa655870
radimous <jurda92@gmail.com>
2024-12-15 12:38:00 +0100

    save transform

--- src/components/layout/PanZoom.vue
+++ src/components/layout/PanZoom.vue
@@ -7,9 +7,13 @@
 <script setup>
 import panzoom from 'panzoom'
 import { ref, onMounted, onUnmounted } from 'vue'
+import { save, get } from '@/util/save'
 
 const element = ref(null)
 let instance = null
+let trX = null
+let trY = null
+let trScale = null
 
 onMounted(() => {
   instance = panzoom(element.value, {
@@ -21,7 +25,38 @@ onMounted(() => {
     },
     autocenter: true
   })
+  loadTransform();
+  periodicallySaveTransform();
+  console.log(instance)
 })
+
+const setTransform = (transform) => {
+  instance.getTransform().x = transform.x
+  instance.getTransform().y = transform.y
+  instance.getTransform().scale = transform.scale
+
+  trX = transform.x
+  trY = transform.y
+  trScale = transform.scale
+}
+
+const periodicallySaveTransform = () => {
+  setInterval(() => {
+    let trsfrm = instance.getTransform()
+    // don't save if there is no change
+    if (trX === trsfrm.x && trY === trsfrm.y && trScale === trsfrm.scale) {
+      return;
+    }
+    save('transform', trsfrm)
+  }, 1000) 
+}
+
+const loadTransform = () => {
+  const transform = get('transform')
+  if (transform) {
+    setTransform(transform)
+  }
+}
 
 onUnmounted(() => {
   instance.dispose()
